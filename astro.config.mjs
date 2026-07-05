98327dcf19389c80655c436f56319c648e89c564
OathPros <144079709+OathPros@users.noreply.github.com>
2026-07-05 09:07:26 -0400

    Fix GitHub Pages styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://danielfry3.github.io',
+  site: 'https://oathpros.github.io',
   base: '/D-CB',
   output: 'static',
+  build: {
+    inlineStylesheets: 'always',
+  },
 });
